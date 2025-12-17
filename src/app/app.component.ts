2ec46adc18006171fd64a8ecdff70ea25f20ea31
wipsyofc <wipsymambue@gmail.com>
2025-12-17 14:45:55 +0200

    revision du footer et du header pour les inclures dans le app-root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent,
+    FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
