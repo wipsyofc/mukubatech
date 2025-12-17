29c6443687c10aac90ad3604ea7f91f83c8585e9
wipsyofc <wipsymambue@gmail.com>
2025-12-17 12:36:41 +0200

    mis à jour de la page qui sommes nous

--- src/app/apropos/apropos.component.ts
+++ src/app/apropos/apropos.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-apropos',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './apropos.component.html',
   styleUrl: './apropos.component.css'
 })
--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,33 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+
+//-------------------------------- BOUTON REVENIR EN HAUT DE LA PAGE -------------------------------//
+const myButton = document.getElementById("btn-back-to-top");
+
+if (!myButton) {
+  throw new Error("Le bouton btn-back-to-top est introuvable");
+}
+
+const button = myButton as HTMLElement;
+
+// Scroll
+window.addEventListener("scroll", (): void => {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+});
+
+// Click
+button.addEventListener("click", (): void => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+//-------------------------------- FIN BOUTON REVENIR EN HAUT DE LA PAGE -------------------------------//  
